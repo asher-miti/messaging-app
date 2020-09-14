ab71bba93b18b952c65d64352d5fdd284d72e157
Asher <asher_3@hotmail.co.uk>
2020-09-14 17:49:00 +0100

    created login page along with custom localstorage hook for storage details

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Login from "./Login";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
+
+const App = () => {
+  const [id, setId] = useLocalStorage();
+  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+};
+
+export default App;
